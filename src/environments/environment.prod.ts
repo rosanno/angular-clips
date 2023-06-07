a365dab53a0dc99ee6fd7f44c3714ff87b3956ca
rosanno <rosannomacabuhay@gmail.com>
2023-06-07 20:29:09 +0800

    Initial

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyASCxHgCSpmVnQwVbLCHATcns5fJQFdmpY",
+    authDomain: "clips-6e37e.firebaseapp.com",
+    projectId: "clips-36548",
+    storageBucket: "clips-36548.appspot.com",
+    appId: "1:798864185458:web:f93bd21de7f956bffc4aeb"
+  }
+};
+
+// apiKey: "AIzaSyBdfgPtZi9mGaqxJlXbnVnQkY-PQbhBGzc",
+// authDomain: "clips-6e37e.firebaseapp.com",
+// projectId: "clips-6e37e",
+// storageBucket: "clips-6e37e.appspot.com",
+// messagingSenderId: "419979862810",
+// appId: "1:419979862810:web:16b513f106f8cc2ea502a3"
